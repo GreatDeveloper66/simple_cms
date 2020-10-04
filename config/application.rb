a7c2f79f1a8ccbf028a1eac7e031432dbc877ca4
GreatDeveloper66 <ashaffe@gmail.com>
2020-10-04 13:42:40 -0500

    update system files for dotenv-rails

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
+
 require_relative 'boot'
 
 require 'rails/all'
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,6 +11,7 @@ Bundler.require(*Rails.groups)
 module SimpleCms
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+
     config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
